f02bfb501372bbc274444f85910b2748fb4a603e
Daniela Avila Reyes <aviladanielap@gmail.com>
2022-07-27 15:47:16 -0300

    prueba

--- src/componentes/servicios/firebase/index.js
+++ src/componentes/servicios/firebase/index.js
@@ -4,11 +4,11 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey:process.env.REACT_APP_apiKey ,
-  authDomain:process.env.REACT_APP_authDomain ,
-  projectId:process.env.REACT_APP_projectId ,
-  storageBucket:process.env.REACT_APP_storageBucket ,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
   messagingSenderId:process.env.REACT_APP_messagingSenderId, 
-  appId:process.env.REACT_APP_appId
+  appId:process.REACT_APP_appId,
 };
 
 // Initialize Firebase
